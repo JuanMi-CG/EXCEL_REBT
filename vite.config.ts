96e02cb6f7a3a567af903a933fb0975e8bd4e01c
Juan Miguel <jm.coll.gonzalez@gmail.com>
2025-03-20 23:44:33 +0100

    Deploy!!

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/EXCEL_REBT',
+  base: '/EXCEL_REBT/',
   plugins: [react()],
   resolve: {
     alias: {
